e3236b557ca34a144ee319a3958b787fa964b09a
Derrick Hammer <derrick@derrickhammer.com>
2023-10-09 09:14:50 -0400

    chore: delete dist

--- dist/src/components/lume/LumeDashboard/LumeDashboard.d.ts
+++
@@ -1,2 +0,0 @@
-declare const LumeDashboard: () => import("react/jsx-runtime").JSX.Element;
-export default LumeDashboard;
--- dist/src/main.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './components/lume/LumeProvider';
-export * from './components/lume/LumeDashboard/LumeDashboard';
-export * from './components/lume/LumeIdentity/LumeIdentity';
